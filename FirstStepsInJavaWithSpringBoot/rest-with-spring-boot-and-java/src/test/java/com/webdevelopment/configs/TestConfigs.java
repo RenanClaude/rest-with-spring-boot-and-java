7e9056a83d0ccb59244e0e64402eb6f82f345cc2
Renan Claude <renanclaude.rcg@gmail.com>
2023-11-12 23:14:30 -0300

    Integration testing for CORS

--- FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/com/webdevelopment/configs/TestConfigs.java
+++ FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/com/webdevelopment/configs/TestConfigs.java
@@ -4,5 +4,14 @@ public class TestConfigs {
 	
 	// Porta padrão que o rest-assured irá utilizar para se conectar aos testes
 	public static final int SERVER_PORT = 8888;
+	
+	public static final String HEADER_PARAM_ORIGIN_AUTHORIZATION = "Authorization";
+	
+	public static final String HEADER_PARAM_ORIGIN = "Origin";
+	
+	public static final String CONTENT_TYPE_JSON = "application/json";
+	public static final String CONTENT_TYPE_XML = "application/xml";
+	public static final String CONTENT_TYPE_YML = "application/yaml";
+	
 
 }
