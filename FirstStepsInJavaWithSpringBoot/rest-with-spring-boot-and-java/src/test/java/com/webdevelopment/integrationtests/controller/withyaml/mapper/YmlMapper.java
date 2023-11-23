abbade3d619993d795786e43f6fdc190b05b2216
Renan Claude <renanclaude.rcg@gmail.com>
2023-11-22 23:38:39 -0300

    Implementing YmlMapper for tests

---
+++ FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/com/webdevelopment/integrationtests/controller/withyaml/mapper/YmlMapper.java
@@ -0,0 +1,50 @@
+package com.webdevelopment.integrationtests.controller.withyaml.mapper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+
+import io.restassured.mapper.ObjectMapper;
+import io.restassured.mapper.ObjectMapperDeserializationContext;
+import io.restassured.mapper.ObjectMapperSerializationContext;
+
+public class YmlMapper implements ObjectMapper {
+
+	private com.fasterxml.jackson.databind.ObjectMapper objectMapper;
+	protected TypeFactory typeFactory;
+
+	public YmlMapper(TypeFactory typeFactory) {
+		this.objectMapper = new com.fasterxml.jackson.databind.ObjectMapper();
+		objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		this.typeFactory = typeFactory.defaultInstance();
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public Object deserialize(ObjectMapperDeserializationContext context) {
+		try {
+			String dataToDeserialize = context.getDataToDeserialize().asString();
+
+			Class type = (Class) context.getType();
+			return objectMapper.readValue(dataToDeserialize, typeFactory.constructType(type));
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	@Override
+	public Object serialize(ObjectMapperSerializationContext context) {
+		try {
+			return objectMapper.writeValueAsString(context.getObjectToSerialize());
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
