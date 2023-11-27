8a01f7cecfaea0c03c38f806408c2b169acf5cf7
Renan Claude <renanclaude.rcg@gmail.com>
2023-11-27 02:59:47 -0300

    Adding enablePerson to PersonController + Refactoring query at PersonRepository

--- FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/com/webdevelopment/controllers/PersonController.java
+++ FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/com/webdevelopment/controllers/PersonController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -111,6 +112,25 @@ public class PersonController {
 		PersonVO updatedPerson = this.personServices.update(person);
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(updatedPerson);
+
+	}
+	
+	@PatchMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
+			MediaType.APPLICATION_YML })
+	@Operation(summary = "Disable a specifc Person by ID", description = "Disable a specifc Person by ID", tags = { "People" },
+	responses = {
+			@ApiResponse(description = "Success", responseCode = "200", content =
+					@Content(schema = @Schema(implementation = PersonVO.class))),
+			@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+			})
+	public ResponseEntity<PersonVO> disablePerson(@PathVariable(value = "id") Long id) {
+		PersonVO person = this.personServices.disablePerson(id);
+
+		return ResponseEntity.status(HttpStatus.OK).body(person);
 
 	}
 
