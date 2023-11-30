fbdbdd6bb6fb42549eef596c77e1a33e89bb3cda
Renan Claude <renanclaude.rcg@gmail.com>
2023-11-30 19:46:28 -0300

    Implementing FileStorageServices

---
+++ FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/com/webdevelopment/services/FileStorageServices.java
@@ -0,0 +1,53 @@
+package com.webdevelopment.services;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.webdevelopment.config.FileStorageConfig;
+import com.webdevelopment.exceptions.FileStorageException;
+
+@Service
+public class FileStorageServices {
+
+	private final Path fileStorageLocation;
+
+	@Autowired
+	public FileStorageServices(FileStorageConfig fileStorageConfig) {
+		Path path = Paths.get(fileStorageConfig.getUploadDir()).toAbsolutePath().normalize();
+
+		this.fileStorageLocation = path;
+
+		try {
+			Files.createDirectories(this.fileStorageLocation);
+
+		} catch (Exception e) {
+			throw new FileStorageException("Could not create the directory where the uploaded files will be storage",
+					e);
+		}
+	}
+
+	public String storeFile(MultipartFile file) {
+		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+		try {
+			if (fileName.contains("..")) {
+				throw new FileStorageException("Sorry, filename contains invalid path sequence: " + fileName);
+			}
+			Path targetLocation = this.fileStorageLocation.resolve(fileName);
+			Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+
+			return fileName;
+			
+		} catch (Exception e) {
+			throw new FileStorageException("Could not store file " + fileName + ". Please, try again!", e);
+		}
+	}
+
+}
